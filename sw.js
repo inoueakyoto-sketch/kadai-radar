bd21dd7448b719c8915e2c47994ac986f3fd4fe9
inoueakyoto-sketch <inoueakyoto-sketch@users.noreply.github.com>
2026-09-15 09:14:05 +0000

    deploy: 57c2b1c31754567b8e32563aaeebb72452abe8e8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kadai-radar-v0.6.4";
+const CACHE = "kadai-radar-v0.6.5";
 
 const scopedUrl = (path = "") => new URL(path, self.registration.scope).href;
 const SHELL = [scopedUrl(""), scopedUrl("manifest.webmanifest"), scopedUrl("icon.svg")];
